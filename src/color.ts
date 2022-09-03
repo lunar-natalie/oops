87c690c51932674e45998af8bfcfcd1cb11ed692
Natalie Wiggins <islifepeachy@outlook.com>
2022-09-03 04:21:09 +0100

    chore: remove extraneous class definitions

--- src/color.ts
+++ src/color.ts
@@ -6,14 +6,10 @@ export interface ColorRGB {
     green: number;
     blue: number;
 }
-export class ColorRGB implements ColorRGB {
-}
 
 /**
  * Red, green, blue, and alpha color attributes for canvas objects.
  */
 export interface ColorRGBA extends ColorRGB {
     alpha?: number;
-}
-export class ColorRGBA implements ColorRGBA {
 }
